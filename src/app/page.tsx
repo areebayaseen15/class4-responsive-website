94bcf3999df9e2b9a85a270b2c50b42c4cce6eac
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-11-23 02:57:28 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+
+import Brands from "./components/brands";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Header from "./components/Hero";
+import Navbar from "./components/navbar";
+import Product from "./components/product";
+import Reviews from "./components/Reviws";
+import Dressstyle from "./dressstyle";
+
+
+
+
+
+
+export default function Home(){
+  return(
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Brands/>
+      <Product/>
+      <Dressstyle/>
+      <Reviews/>
+     <Footer/>
+    </div>
+  )
+}
